6ef7d1205e46c28033250636d91eded931156925
Ubuntu <ubuntu@ip-172-31-93-239.ec2.internal>
2025-09-18 09:53:37 +0000

    Added React web app files properly

---
+++ examples/react-web-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ 	<h1>Hello Learners! This is a Demo of using React and Docker Application </h1> 	
+   	<p>Learning Docker and containerize the React App in this Demo</p>
+    </div>
+  );
+}
+
+export default App;
